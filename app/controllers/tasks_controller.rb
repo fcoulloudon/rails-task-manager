316598a4674d5c581f78482a3eb5f3583dbf2ce1
Francois Coulloudon <fcoulloudon@gmail.com>
2021-04-10 06:30:40 +0200

    Exercise 2 done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(restaurant_params_create)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(restaurant_params_modify)
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to task_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    # no need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+  private
+
+  def restaurant_params_create
+    params.require(:task).permit(:title, :details)
+  end
+
+  def restaurant_params_modify
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "tasks", to: "tasks#index"
+
+  # Create
+  get "tasks/new", to: "tasks#new"
+
+  # Read (one)
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # Create task
+  post "tasks", to: "tasks#create"
+
+  # Edit task (get form)
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+
+  # Patch task
+  patch "tasks/:id", to: "tasks#update"
+
+  # Delete tasks
+  delete "tasks/:id", to: "tasks#destroy"
+
 end
